74ed147e41679759c8ade93e9bac36b4e3b3efb7
Mukeshkumar Labana <Mukeshlabana844@gmail.com>
2025-12-29 09:06:23 +0530

    login user implemented

---
+++ src/main/java/in/mk/main/config/security/CustomUserDetails.java
@@ -0,0 +1,52 @@
+package in.mk.main.config.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import in.mk.main.entity.User;
+
+public class CustomUserDetails implements UserDetails {
+	
+	
+	
+	private User user;
+	
+	
+	
+
+	public CustomUserDetails(User user) {
+		super();
+		this.user=user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		List<SimpleGrantedAuthority> authorities =new ArrayList<>();
+		user.getRoles().forEach(r->{
+			authorities.add(new SimpleGrantedAuthority(r.getName()));
+		});
+		
+		
+		
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		
+		return user.getEmail();
+	}
+
+}
